0083273a3ec19ce1024cab56b57e0a3834c41a45
Olawale Ayodeji <56791563+oayodeji@users.noreply.github.com>
2022-10-14 16:57:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def take_command():
         pass
     return command
 
-def run_jarvis():
+def jarvis():
     command = take_command()
     if 'play' in command:
         song = command.replace('play', '')
@@ -59,5 +59,5 @@ def run_jarvis():
 
 if __name__ == "__main__":
   while True and take_command() != "goodbye":
-    run_jarvis()
+    jarvis()
     time.sleep(10)
